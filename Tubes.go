6c229e5a5f71f26e066a0d9d86642cdbd53d4630
Mimir <119404804+M1m1rr@users.noreply.github.com>
2025-05-14 11:45:41 +0700

    Initial commit

---
+++ Tubes.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+const NMAX int = 999
+
+type tabDat [NMAX]int
+
+//Fungsi utama program
+func main() {
+	login()
+	fmt.Scan()
+}
+
+//Fungsi menginput data
+func inputData() {
+
+}
+
+func login() {
+
+}
